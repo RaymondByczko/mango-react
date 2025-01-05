d7073de89dbaaca3a3593993f37c346f36f9ce13
Raymond Byczko <raymondbyczko@gmail.com>
2025-01-05 09:59:33 -0500

    RByczko;2025-01-25;Draft 6. Worked on MyGridEfficient

--- src/MyGridEfficient.js
+++ src/MyGridEfficient.js
@@ -1,19 +1,19 @@
 // MyGridEfficient
 import { useState } from 'react';
 import React from 'react';
+function do_a_for() {
+		let y=2;
+		for ( let i=0; i <= 7; i++ ) {
+			// output = output + "<div className='wrapperElement'>{content[i]}</div>";
+			y = y + i;
+		}
+		return y;
+}
 export default function MyGridEfficient({content}) {
-	let lenContent = content.length;
-	console.log(`lenContent=${lenContent}`);
-	return (
-		<div className="wrapper">
-		<div className="wrapperElement">{content[0]}</div>
-		<div className="wrapperElement">{content[1]}</div>
-		<div className="wrapperElement">{content[2]}</div>
-		<div className="wrapperElement">{content[3]}</div>
-		<div className="wrapperElement">{content[4]}</div>
-		<div className="wrapperElement">{content[5]}</div>
-		<div className="wrapperElement">{content[6]}</div>
-		<div className="wrapperElement">{content[7]}</div>
-		</div>
-	);
+	const outputArray = [];
+	outputArray.push(<div key={100} className='wrapper'></div>);
+	for (let i = 0; i<content.length; i++) {
+		outputArray.push(<div key={i} className='wrapperElement'>{content[i]}</div>);
+	}
+	return ( <div>{outputArray}</div> );
 };
